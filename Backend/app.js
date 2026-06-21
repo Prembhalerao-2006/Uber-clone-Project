e537eba10dbb0ad551bd0c03d6279b1b2c1996aa
Prembhalerao-2006 <prembhalerao2022@gmail.com>
2026-06-21 14:39:31 +0530

    Added: Get Profile API

--- Backend/app.js
+++ Backend/app.js
@@ -3,7 +3,7 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 connectToDb();
 
 app.use(cors());
-
+app.use(cookieParser());
 app.get('/', (req, res)=>{
     res.send('hello world');
 });
---
+++ Backend/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const blacklistTokenModel = require("../models/blacklistToken.model");
+
+
+module.exports.authUser = async (req, res, next) => {
+   const token = req.cookies.token || req.headers.authorization?.split(' ')[1] ;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. Unauthorized.' });
+    }
+    // try {
+    //     const isBlacklisted = await blacklistTokenModel.findOne({ token });
+    //     if (isBlacklisted) {
+    //         return res.status(401).json({ message: 'Unauthorized.' });
+    //     }
+    // } catch (err) {
+    //     console.error('[auth] blacklist check failed:', err && err.message);
+    //     // continue - blacklist check failure shouldn't block valid tokens by default
+    // }
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await userModel.findById(decoded._id);
+    
+        req.user = user;
+         
+        return next();
+    }
+    catch(error) {
+    
+        return res.status(401).json({ message: 'Unauthorized.' });
+    }
+}
